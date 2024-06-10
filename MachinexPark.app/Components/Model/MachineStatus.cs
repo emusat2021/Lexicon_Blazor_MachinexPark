f7146f520609f0fff28ab71a6e41431bf975bad8
Eugenia <musat.eugenia@gmail.com>
2024-06-10 16:09:07 +0200

    Create Machine model

---
+++ MachinexPark.app/Components/Model/MachineStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace MachinexPark.app.Components.Model
+{
+    public class Machine
+    {
+        public int MachineId { get; set; }
+        public string MachineName { get; set; }
+        public string MachineStatus { get; set; }
+        public DateTime LatestDateFromMachine { get; set; }
+    }
+}
