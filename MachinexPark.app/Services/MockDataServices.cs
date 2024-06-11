615749dac6da6ec3a2b438f7453e3212be8b9f1d
Eugenia <musat.eugenia@gmail.com>
2024-06-11 14:58:00 +0200

    Add Mock data and show it in page for UI

---
+++ MachinexPark.app/Services/MockDataServices.cs
@@ -0,0 +1,45 @@
+﻿
+using MachinexPark.app.Model;
+
+namespace MachinexPark.app.Services
+{
+    public class MockDataServices
+    {
+        private static List<Machine> _machines = default!;
+
+        public static List<Machine> Machines
+        {
+            get
+            {
+                _machines ??= InitializeMockMachines();
+                return _machines;
+            }
+        }
+
+        private static List<Machine> InitializeMockMachines()
+        {
+            var m1 = new Machine
+            {
+                MachineId = 1,
+                MachineName = "HydraPress 3000",
+                MachineStatus = false,
+                LatestDateFromMachine = new DateTime(2024, 6, 11),
+            };
+            var m2 = new Machine
+            {
+                MachineId = 2,
+                MachineName = "AutoCNC MillPro",
+                MachineStatus = false,
+                LatestDateFromMachine = new DateTime(2024, 6, 11),
+            };
+            var m3 = new Machine
+            {
+                MachineId = 3,
+                MachineName = "RoboWeld XR7",
+                MachineStatus = false,
+                LatestDateFromMachine = new DateTime(2024, 6, 11),
+            };
+            return new List<Machine> { m1, m2, m3 };
+        }
+    }
+}
