c7a3327133eb43b22edfd6c3f225ccb40b8a0f00
Eugenia <musat.eugenia@gmail.com>
2024-06-13 16:29:52 +0200

    Change dateTime in mockData

--- MachinexPark.app/Services/MockDataServices.cs
+++ MachinexPark.app/Services/MockDataServices.cs
@@ -30,21 +30,21 @@ namespace MachinexPark.app.Services
                 MachineId = 2,
                 MachineName = "AutoCNC MillPro",
                 MachineStatus = false,
-                LatestDateFromMachine = new DateTime(2024, 6, 11),
+                LatestDateFromMachine = new DateTime(2024, 6, 12),
             };
             var m3 = new Machine
             {
                 MachineId = 3,
                 MachineName = "RoboWeld XR7",
                 MachineStatus = false,
-                LatestDateFromMachine = new DateTime(2024, 6, 11),
+                LatestDateFromMachine = new DateTime(2024, 6, 13),
             };
             var m4 = new Machine
             {
                 MachineId = 4,
                 MachineName = "Industrial Robotic Arm",
                 MachineStatus = false,
-                LatestDateFromMachine = new DateTime(2024, 6, 11),
+                LatestDateFromMachine = new DateTime(2024, 6, 13),
             };
             return new List<Machine> { m1, m2, m3, m4 };
         }
